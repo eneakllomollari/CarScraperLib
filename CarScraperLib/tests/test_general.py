f464f8b72a259290b9feca0ece5a4b02bc1bd8d7
Enea Kllomollari <ekllomollari@ucdavis.edu>
2019-10-20 13:59:32 -0700

    Add general test

---
+++ CarScraperLib/tests/test_general.py
@@ -0,0 +1,7 @@
+def test_general():
+    # noinspection PyUnresolvedReferences
+    from .. import classes
+    # noinspection PyUnresolvedReferences
+    from .. import consts
+    # noinspection PyUnresolvedReferences
+    from .. import utils
