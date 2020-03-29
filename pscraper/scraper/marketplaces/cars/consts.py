75ab57320c32cd2abdc89e7ef0bd12351eb1b706
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-03-29 05:20:51 -0700

    Search for localOnly dealers

--- pscraper/scraper/marketplaces/cars/consts.py
+++ pscraper/scraper/marketplaces/cars/consts.py
@@ -1,4 +1,4 @@
-CARS_COM_QUERY = 'https://www.cars.com/for-sale/searchresults.action/?fuelTypeId=38745&' \
+CARS_COM_QUERY = 'https://www.cars.com/for-sale/searchresults.action/?dealerType=localOnly&fuelTypeId=38745&' \
                  'page={}&perPage=100&rd={}&zc={}&userSetIxt=true'
 PAGE = 'page'
 SEARCH = 'search'
