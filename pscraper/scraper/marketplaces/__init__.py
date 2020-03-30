876673f5c45d9960af517bd3307953a34168c0c7
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-03-30 15:17:04 -0700

    Don't post message to slack after scraping is complete
    Leave it to the scraping tool

--- pscraper/scraper/marketplaces/__init__.py
+++ pscraper/scraper/marketplaces/__init__.py
@@ -1,4 +1,3 @@
-from pscraper.utils.report import post_daily_slack_report
 from .autotrader import scrape_autotrader
 from .carmax import scrape_carmax
 from .cars import scrape_cars
@@ -6,23 +5,19 @@ from .cars import scrape_cars
 
 def scrape(zip_code, search_radius, target_states, api):
     """ Scrape data about electric vehicles from all supported marketplaces using the specified parameters
-    You need credentials with admin permissions to be set to environment variables
-     `PSCRAPER_USERNAME` and `PSCRAPER_PASSWORD`
 
     Args:
         zip_code (str): The zip code to perform the search in
-        search_radius (str): The search radius for the specified zip code
+        search_radius (int): The search radius for the specified zip code
         target_states (list): The states to search in (i.e. ```['CA', 'NV']```)
         api (pscraper.api.API): Pscraper API to communicate with the DB
 
     Returns:
-        total: Number of scraped cars per marketplaces (cars.com, autotrader, carmax)
+        list of tuples: (elapsed time and total vehicles) per marketplace
 
     """
     cars_et, cars_total = scrape_cars(zip_code, search_radius, target_states, api)
     autotrader_et, autotrader_total = scrape_autotrader(zip_code, search_radius, target_states, api)
     carmax_et, carmax_total = scrape_carmax(zip_code, search_radius, target_states, api)
 
-    post_daily_slack_report(cars_et, cars_total, autotrader_et, autotrader_total, carmax_et, carmax_total)
-
-    return cars_total, autotrader_total, carmax_total
+    return (cars_et, cars_total), (autotrader_et, autotrader_total), (carmax_et, carmax_total)
