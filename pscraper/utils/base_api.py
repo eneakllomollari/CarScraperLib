1fe7ce1a3b3143d42ad295ee47d910e81245cf8e
Enea Kllomollari <ekllomollari@ucdavis.edu>
2020-04-12 20:49:53 -0700

    Add logging and update error handling in base_api

--- pscraper/utils/base_api.py
+++ pscraper/utils/base_api.py
@@ -1,10 +1,14 @@
+import logging
 from functools import wraps
+from json import JSONDecodeError
 
-import requests
 from hamcrest import assert_that, equal_to, is_in
+from requests.exceptions import ConnectionError, RequestException
 from requests.sessions import Session
 
 from .misc import get_traceback, send_slack_message
+
+logger = logging.getLogger(__name__)
 
 
 def request_wrapper(method, success_codes):
@@ -12,18 +16,22 @@ def request_wrapper(method, success_codes):
         @wraps(func)
         def wrapper(self, url, *args, **kwargs):
             url = self.get_full_url(url)
+            logger.info(f'Request: {url} {args if args else ""}{kwargs if kwargs else ""}')
             try:
                 resp = func(self, url, *args, **kwargs)
+                logger.info(f'Response: {resp.status_code} {resp.text}')
                 assert_func = is_in if type(success_codes) is list else equal_to
-                assert_that(resp.status_code, assert_func(success_codes), f'```{method} {url} failed with status code: '
-                                                                          f'{resp.status_code}\n'
-                                                                          f'Request: {args if args else ""}{kwargs}\n'
-                                                                          f'Response: {resp.json()}```')
+                error_msg = f'```{method} {url} failed with status code: {resp.status_code}\n' \
+                            f'Request: {args if args else ""}{kwargs}\nResponse: {resp.json()}'
+                assert_that(resp.status_code, assert_func(success_codes), error_msg)
                 return resp.json()
-            except (requests.exceptions.RequestException, AssertionError):
-                send_slack_message(channel='#errors', text=f'```{get_traceback()}```')
+            except (RequestException, ConnectionError, AssertionError, JSONDecodeError):
+                logger.debug(f'{method} {url} failed!', exc_info=get_traceback())
+                send_slack_message(channel='#errors')
             return -1
+
         return wrapper
+
     return decorator
 
 
